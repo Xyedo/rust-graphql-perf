f1b327a6fdffcd1797b3aa905cec09a5e5de3ebc
Luke Mauldin <luke.mauldin@zaelot.com>
2023-05-09 14:05:40 -0500

    initial working graphql implementation

---
+++ src/main.rs
@@ -0,0 +1,99 @@
+use async_graphql::{
+    Object,
+    http::{playground_source, GraphQLPlaygroundConfig},
+    EmptyMutation, EmptySubscription, ObjectType, Request, Response, Schema,
+    SubscriptionType,
+};
+use axum::{
+    extract::Extension,
+    response::{Html, IntoResponse},
+    http::{HeaderMap},
+    routing::get,
+    Router, Server,
+};
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+
+#[derive(Clone)]
+struct Query;
+
+#[Object]
+impl Query {
+  async fn howdy(&self) -> &'static str {
+    "partner"
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error>{
+    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let app = App{schema};
+    std::fs::write("schema.graphql", app.sdl())?;
+
+    let graphql_http = Router::new()
+        .route(
+            "/graphql",
+            get(graphql_playground).post(graphql_handler::<Query, EmptyMutation, EmptySubscription>),
+        )
+        .layer(Extension(app));
+    let health = Router::new().route("/health", get(health));
+
+    let graphql_http = graphql_http.merge(health);
+
+    println!("Server started on port {}", "8080");
+
+    Server::bind(&([0, 0, 0, 0], 8080).into())
+        .serve(graphql_http.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Clone)]
+pub struct App<TQuery, TMutation, TSubscription>
+where
+    TQuery: ObjectType + Sized + 'static,
+    TMutation: ObjectType + Sized + 'static,
+    TSubscription: SubscriptionType + Sized + 'static,
+{
+    schema: Schema<TQuery, TMutation, TSubscription>,
+}
+
+#[allow(clippy::type_complexity)]
+impl<TQuery, TMutation, TSubscription> App<TQuery, TMutation, TSubscription>
+where
+    TQuery: ObjectType + Sized + 'static,
+    TMutation: ObjectType + Sized + 'static,
+    TSubscription: SubscriptionType + Sized + 'static,
+{
+    pub fn sdl(&self) -> String {
+        self.schema.sdl()
+    }
+
+    pub async fn execute(&self, _: HeaderMap, req: Request) -> Response {
+        self.schema.execute(req).await
+    }
+
+}
+
+pub async fn graphql_handler<TQuery, TMutation, TSubscription>(
+    Extension(app): Extension<App<TQuery, TMutation, TSubscription>>,
+    headers: HeaderMap,
+    req: GraphQLRequest,
+) -> GraphQLResponse
+where
+    TQuery: ObjectType + Sized + 'static,
+    TMutation: ObjectType + Sized + 'static,
+    TSubscription: SubscriptionType + Sized + 'static,
+{
+    app.execute(headers, req.into_inner()).await.into()
+}
+
+#[allow(clippy::unused_async)]
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new("/")))
+}
+
+#[allow(clippy::unused_async)]
+async fn health() -> Html<&'static str> {
+    Html("Ok")
+}
